cd3f5703f10db3f6721a0d74d9668cbee5d5f9c5
unknown <ryanjaytagolimotreyes@gmail.com>
2025-05-06 20:51:49 +0800

    f

--- src/views/user/AddMember.tsx
+++ src/views/user/AddMember.tsx
@@ -193,11 +193,10 @@ const AddMember: React.FC = () => {
                           <TableCell className="flex justify-center items-center">
                             {member.photo ? (
                               <img
-                              src={`${import.meta.env.VITE_URL}/storage/${member.photo}`}
-                              alt={member.name || 'Member Photo'}
-                              className="rounded-full h-10 w-10"
-                              onError={(e) => (e.currentTarget.src = '/fallback-image.png')} // Optional fallback image
-                            />
+                                src={`${import.meta.env.VITE_URL}/storage/${member.photo}`}
+                                alt={member.name || 'Member Photo'}
+                                className="rounded-full h-10 w-10"
+                              />
                             ) : (
                               <Avatar>
                                 <AvatarImage src="https://github.com/shadcn.png" />
